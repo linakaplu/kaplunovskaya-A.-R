a986b747905497796baa507c8050ee1f8c190999
linakaplu <91316452+linakaplu@users.noreply.github.com>
2021-10-05 19:35:56 +0300

    Update 1.4.py

--- 1.4.py
+++ 1.4.py
@@ -1,8 +1,7 @@
-# -- coding: utf-8 -- 
-seconds=int(input('Введите количество секунд:'))
-if seconds>60:
-    print((seconds//60), 'минут')
-if seconds>3600:
-    print((seconds//3600), 'часов')
-if seconds>86400:
-    print((seconds//86400), 'дней')
+# -- coding: utf-8 --
+seconds = int(input("Введите количество секунд: "))
+day = seconds // 86400
+hour = seconds // 60 // 60 % 24
+min = seconds // 60 % 60
+sec = seconds % 60
+print (day,'дней',hour,'часов',min,'минут',sec)
