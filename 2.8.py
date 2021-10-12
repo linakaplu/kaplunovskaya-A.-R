82b5787b9a82c648514eb52b44f109f925e2816d
linakaplu <91316452+linakaplu@users.noreply.github.com>
2021-10-12 15:10:24 +0300

    Delete 2.8.py

--- 2.8.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-def add (a, b, c):
-    if a==b and a==c:
-        return ('3')
-    if a==b or a==c or b==c:
-        return ('2')
-    else:
-        return ('0')
-print (add (int(input('a: ')), int(input('b: ')), int(input('c: '))))
