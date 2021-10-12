b83e8121fa6a98e71d8b037939608dfc69331870
linakaplu <91316452+linakaplu@users.noreply.github.com>
2021-10-12 15:12:02 +0300

    Add files via upload

---
+++ zadan 2/2.1.py
@@ -0,0 +1,6 @@
+ # -- coding: utf-8 --
+print('даны три числа:')
+x=int(input())
+y=int(input())
+z=int(input())
+print(x + y + z)
---
+++ zadan 2/2.2.py
@@ -0,0 +1,5 @@
+# -- coding: utf-8 --
+first=int(input('первый катет:'))
+second=int(input('второй катет:'))
+S = 0.5 * first * second
+print ('Площадь треугольника равна:', S)
