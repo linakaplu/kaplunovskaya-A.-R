17f9bdc06139f28e659d322f3ebe1cb96ea0984f
linakaplu <91316452+linakaplu@users.noreply.github.com>
2021-10-12 15:09:43 +0300

    Delete 2.5.py

--- 2.5.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-def add (a, b, c):
-    if a<=b and a<=c:
-        return a 
-    if b<=a and b<=c:
-        return b
-    else:
-        return c 
-print(add (int(input('число a:')), int(input('число b:')), int(input('число c:'))))
