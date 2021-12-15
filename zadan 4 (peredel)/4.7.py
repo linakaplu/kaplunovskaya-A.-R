80d15c3f4890679dfca34c558c69c4c6bee13f84
linakaplu <91316452+linakaplu@users.noreply.github.com>
2021-12-15 10:50:39 +0300

    Delete 4.7.py

--- zadan 4 (peredel)/4.7.py
+++
@@ -1,9 +0,0 @@
-#--coding:utf-8 --
-def n():
-    s = (input("строка "))
-    if s.count ('h')>1:
-        k=(s[:s.find('h')] + s[s.rfind('h')+1:])
-        print (k)
-    else:
-        print ("букв h должно быть больше")
-n()
