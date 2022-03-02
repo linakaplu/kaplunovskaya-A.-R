adf52ac9a0cdcee1e361e2eaab88ab805e7b2975
linakaplu <91316452+linakaplu@users.noreply.github.com>
2022-03-02 18:35:42 +0300

    Add files via upload

---
+++ Java/1pr/zadan3.java
@@ -0,0 +1,14 @@
+package zadan3;
+
+public class zadan3 {
+	public class Test {
+		public static void main(String[] args) {
+			if (args.length != 2) {
+				System.out.println("Неверное количество параметров");
+				return;
+				}
+			System.out.println(args[0] + " + " + args[1] + " = " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
+			}
+		}
+
+}
