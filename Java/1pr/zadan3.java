883f5971c7159724049782b96b609ba00680d20f
linakaplu <91316452+linakaplu@users.noreply.github.com>
2022-03-02 23:36:05 +0300

    Update zadan3.java

--- Java/1pr/zadan3.java
+++ Java/1pr/zadan3.java
@@ -3,7 +3,7 @@ package zadan3;
 public class zadan3 {
 		public static void main(String[] args) {
 			if (args.length != 2) {
-				System.out.println("Íåâåðíîå êîëè÷åñòâî ïàðàìåòðîâ");
+				System.out.println("Incorrect number of parameters");
 				return;
 				}
 			System.out.println(args[0] + " + " + args[1] + " = " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
