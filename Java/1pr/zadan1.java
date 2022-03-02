957542605c38f0f4d2a45cd52085afcd9208c7c4
linakaplu <91316452+linakaplu@users.noreply.github.com>
2022-03-02 18:47:07 +0300

    Add files via upload

---
+++ Java/1pr/zadan1.java
@@ -0,0 +1,15 @@
+package zadan1;
+
+public class zadan1 {
+	public class HelloWorld {
+		public static void main(String[] args) {
+			if (args.length>0) {
+				System.out.println("Hello, "+ args[0]);
+			}
+			else;{
+				System.out.println("Hello, World!");
+			}
+		}
+	}
+
+}
