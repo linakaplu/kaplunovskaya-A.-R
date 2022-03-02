7da47db1416aa701977f5a9e87b59b243b3a4bb2
linakaplu <91316452+linakaplu@users.noreply.github.com>
2022-03-02 18:43:20 +0300

    Add files via upload

---
+++ Java/1pr/zadan2.java
@@ -0,0 +1,19 @@
+package zadan2;
+
+public class zadan2 {
+	public static void main(String arg[])
+	{
+		int n = arg.length;
+		if (n > 0)
+	    {
+	    	System.out.println("вы ввели " + n + " параметров");
+	    	}
+	    else
+	    {
+	    	System.out.println("вы не передавали параметров!");
+	    	}
+	   }
+	}
+
+
+
