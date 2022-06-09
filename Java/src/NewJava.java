eab08129096da19937e7f8b3ce845fcd87f55f8a
linakaplu <91316452+linakaplu@users.noreply.github.com>
2022-06-09 22:27:59 +0300

    Delete NewJava.java

--- Java/src/NewJava.java
+++
@@ -1,7 +0,0 @@
-package PrJava3;
-public class NewJava {
-    public static void main(String[] args) {
-        prjava3 person = new prjava3("МЕХАник", "ул. Кренделькова, 36А", "Mazda X5", "ремонт двигателя, замена масла, заправка кондиционера", 18.06.2020, 24.06.2020, "успешно", "Колесников", 4620);
-        System.out.println(person);
-    }
-}
